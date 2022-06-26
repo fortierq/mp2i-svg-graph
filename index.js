8f72a9868f5274cf356bd30a417fdd4e738130f7
Quentin Fortier <qpfortier@gmail.com>
2022-06-26 22:32:10 +0200

    Add html js

---
+++ index.js
@@ -0,0 +1,13 @@
+import Snap from 'snapsvg';
+
+function draw() {
+    var s = Snap("#svg");
+    // Lets create big circle in the middle:
+    var bigCircle = s.circle(150, 150, 100);
+    // By default its black, lets change its attributes
+    bigCircle.attr({
+        fill: "#bada55",
+        stroke: "#000",
+        strokeWidth: 5
+    });
+}
